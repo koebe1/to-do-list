04f6a4ed7bee8251b8d6d8c8dd1a5bd341945871
koebe1 <koe.be.pay@web.de>
2020-05-10 17:50:30 +0200

    updated to-do

--- script.js
+++ script.js
@@ -1,18 +1,57 @@
 // Create a to do list app
+
+const inputField = document.getElementById("todoInput");
+const list = document.getElementById("todoList");
+const header = document.getElementById('header');
+
+
 const addItem = () => {
-    const inputField = document.getElementById("todoInput");
     const item = document.getElementById("todoInput").value;
     const text = document.createTextNode(item);
     const newItem = document.createElement("li");
-    const list = document.getElementById("todoList");
 
-    newItem.appendChild(text);
-    list.appendChild(newItem);
-    inputField.value = "";
 
+    if (item === "") {
+        showErrorMessage();
+    
+    } else {
+        newItem.appendChild(text);
+        const newList = list.appendChild(newItem);
+        inputField.value = "";
+
+        //save in local storage
+        //localStorage.setItem("todos", item);
+    }
+}
+
+const showErrorMessage = () => {
+    const error = document.createElement("h2");
+    const errorText = document.createTextNode("Please add an item!");
+    error.appendChild(errorText);
+    header.appendChild(error);
 }
 
 const removeItem = () => {
-    const list = document.getElementById('todoList');
-    const remove = list.lastElementChild.remove();
+    const remove = list.lastElementChild;
+    remove.remove();
+    //remove in local storage
+    //localStorage.removeItem("todos", remove);
 }
+
+
+// const onInit = () => {
+//     const inputField = document.getElementById("todoInput");
+//     const list = document.getElementById("todoList");
+//     const item = document.getElementById("todoInput").value;
+//     const li = document.createElement("li");
+//
+//     let toDos = localStorage.todos;
+//     const text = document.createTextNode(toDos);
+//     console.log(toDos);
+//     const newLi = li.appendChild(text);
+//     console.log(newLi);
+//     list.appendChild(newLi)
+//
+// }
+//
+// onInit();
